52ff59a27ce0ca660fce008df1c2ae4fb4714a29
İsmet Kağan Çetin <ikagancetin@gmail.com>
2021-05-10 13:48:39 +0300

    datatable configure
    add filter deleted veriables and column visibilty to users page

--- helpers/handlebarsHelpers.js
+++ helpers/handlebarsHelpers.js
@@ -39,4 +39,23 @@ module.exports = {
   makeLink: function (value, options) {
     return value.replace(/\s/g, "-");
   },
+  convertDateDay: function (value) {
+    let date = new Date(parseInt(value));
+    console.log(date);
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+    let day = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+
+    return day + "/" + month + "/" + year;
+  },
+  convertDateHour: function (value) {
+    let date = new Date(parseInt(value));
+    let hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+    let min = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+    return hour + ":" + min;
+  },
 };
