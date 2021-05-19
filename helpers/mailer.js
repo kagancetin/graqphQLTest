535509f7560bcc53a4014399b9dafb4ff61f42b9
İsmet Kağan Çetin <ikagancetin@gmail.com>
2021-05-19 18:44:10 +0300

    added index some elements

--- helpers/mailer.js
+++ helpers/mailer.js
@@ -1,9 +1,9 @@
-const nodemailer = require("nodemailer")
-const { MailSettings } = require("../models")
+const nodemailer = require("nodemailer");
+const { MailSettings } = require("../models");
 module.exports = {
   sendResetPasswordMail: async (message, callback) => {
-    let settings = await MailSettings.findOne()
-    let url = "localhost:3000/login/resetPassword/" + message
+    let settings = await MailSettings.findOne();
+    let url = "localhost:3000/login/resetPassword/" + message;
 
     let transporter = nodemailer.createTransport({
       host: settings.host,
@@ -14,19 +14,21 @@ module.exports = {
         pass: settings.password, // generated ethereal password
       },
     });
-    let info = await transporter.sendMail({
-      from: '"Meşe Dürüm 👻" <meşedürüm@kurumsal.com>', // sender address
-      to: "ismettestt@gmail.com", // list of receivers
-      subject: "Meşe Dürüm Şifre Yenileme İsteği", // Subject line
-      html: ` 
+    let info = await transporter.sendMail(
+      {
+        from: '"Meşe Dürüm 👻" <meşedürüm@kurumsal.com>', // sender address
+        to: "ikagancetin@gmail.com", // list of receivers
+        subject: "Meşe Dürüm Şifre Yenileme İsteği", // Subject line
+        html: ` 
         <a href="${url}">Şifrenizi Yenilemek için 2 saat içerisinde bu URL\'i kullanabilirsiniz!</a></br><h3>Afiyet Olsun!</h3>
         `,
-    }, (err, info) => {
-      if (err)
-        callback(err, null)
-      else
-        callback(null, info)
-    });
-
-  }
-}
+      },
+      (err, info) => {
+        if (err) {
+          console.log(err);
+          callback(err, null);
+        } else callback(null, info);
+      }
+    );
+  },
+};
